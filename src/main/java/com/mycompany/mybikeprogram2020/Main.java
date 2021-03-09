6c0eac792e7262feb83e097366abe48289004ae9
Darren Stones <dstones@portlaoisecollege.ie>
2021-03-09 11:37:04 +0000

    Upload to Git

--- src/main/java/com/mycompany/mybikeprogram2020/Main.java
+++ src/main/java/com/mycompany/mybikeprogram2020/Main.java
@@ -1,15 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.mycompany.mybikeprogram2020;
-/**
- *
- * @author Student
- */
+
+import javax.swing.JOptionPane;
+
 public class Main extends javax.swing.JFrame {
+    
+    //Declare a bicyle object 
+    Bicycle myBike;
+    
+    //Declare variables to extract info from the form
+    int gear;
+    int speed;
+    int cadence;
+    
+    
+    //flag for form validation
+    //form is assumed to be not properly completed 
+    boolean formCompleted= false;
+    
+    //validate form method 
+    public void validateForm()
+    {
+        //assume form not completed at start
+        formCompleted = false;
+        
+        if (cadenceTxt.getText().equals("")){
+            JOptionPane.showMessageDialog(this,"Please enter the cadence!", 
+                    "Missing info",JOptionPane.INFORMATION_MESSAGE);
+        }
+        else  if (speedTxt.getText().equals("")){
+            JOptionPane.showMessageDialog(this,"Please enter the speed!",
+                    "Missing info",JOptionPane.INFORMATION_MESSAGE);
+        }
+        else  if (gearTxt.getText().equals("")){
+            JOptionPane.showMessageDialog(this,"Please enter the gear!",
+                    "Missing info",JOptionPane.INFORMATION_MESSAGE);
+        }
+        else
+        {
+            //form is completed correctly
+            formCompleted = true;
+        }
+        
+    } //end validateForm method
+    
+    
+    // Extract Form Details Method
+    public void extractFormDetails()
+    {
+                
+    }
+    
+    
 
     /** Creates new form Main */
     public Main() {
@@ -28,24 +69,33 @@ public class Main extends javax.swing.JFrame {
         createWindow = new javax.swing.JFrame();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        cadenceTxt = new javax.swing.JTextField();
+        createBikeBtn = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        speedTxt = new javax.swing.JTextField();
+        gearTxt = new javax.swing.JTextField();
         titleLbl = new javax.swing.JLabel();
         createBtn = new javax.swing.JButton();
         displayBtn = new javax.swing.JButton();
         editBtn = new javax.swing.JButton();
         exitBtn = new javax.swing.JButton();
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        createWindow.setTitle("Create Bike");
+        createWindow.setBackground(new java.awt.Color(255, 255, 255));
+        createWindow.setMinimumSize(new java.awt.Dimension(250, 350));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Create Bicycle:");
 
         jLabel2.setText("Cadence:");
 
-        jButton1.setText("Create Bike");
+        createBikeBtn.setText("Create Bike");
+        createBikeBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                createBikeBtnActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Gear:");
 
@@ -56,27 +106,26 @@ public class Main extends javax.swing.JFrame {
         createWindowLayout.setHorizontalGroup(
             createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(createWindowLayout.createSequentialGroup()
-                .addGap(54, 54, 54)
-                .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addContainerGap(54, Short.MAX_VALUE)
+                .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(createWindowLayout.createSequentialGroup()
+                            .addComponent(jLabel2)
+                            .addGap(78, 78, 78)))
                     .addGroup(createWindowLayout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(createWindowLayout.createSequentialGroup()
-                        .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(createWindowLayout.createSequentialGroup()
-                                    .addComponent(jLabel2)
-                                    .addGap(78, 78, 78)))
-                            .addGroup(createWindowLayout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
-                        .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
-                            .addComponent(jTextField2)
-                            .addComponent(jTextField3))
-                        .addGap(40, 40, 40))))
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(cadenceTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
+                    .addComponent(speedTxt)
+                    .addComponent(gearTxt))
+                .addGap(40, 40, 40))
+            .addGroup(createWindowLayout.createSequentialGroup()
+                .addGap(70, 70, 70)
+                .addComponent(createBikeBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         createWindowLayout.setVerticalGroup(
             createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -86,18 +135,18 @@ public class Main extends javax.swing.JFrame {
                 .addGap(26, 26, 26)
                 .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cadenceTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(speedTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
                 .addGap(26, 26, 26)
                 .addGroup(createWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(gearTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(32, 32, 32))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                .addComponent(createBikeBtn)
+                .addGap(34, 34, 34))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -107,6 +156,11 @@ public class Main extends javax.swing.JFrame {
         titleLbl.setText("Menu:");
 
         createBtn.setText("Create a Bike");
+        createBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                createBtnActionPerformed(evt);
+            }
+        });
 
         displayBtn.setText("Display the Bike");
 
@@ -162,6 +216,32 @@ public class Main extends javax.swing.JFrame {
         
     }//GEN-LAST:event_exitBtnActionPerformed
 
+    private void createBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createBtnActionPerformed
+        
+        //Display the create bike window
+        createWindow.setVisible(true);
+        
+    }//GEN-LAST:event_createBtnActionPerformed
+
+    private void createBikeBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createBikeBtnActionPerformed
+        
+        // validate form to see if completed correctly
+        validateForm();
+        
+        //if form is completed correctly 
+        if (formCompleted) {
+        
+            // extract info from the form
+            extractFormDetails(); 
+            //create a bicycle 
+            myBike = new Bicycle(cadence, speed, gear); 
+            
+            //Test display myBike
+            System.out.println(myBike.toString());
+             
+        } //end if 
+    }//GEN-LAST:event_createBikeBtnActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -198,19 +278,19 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField cadenceTxt;
+    private javax.swing.JButton createBikeBtn;
     private javax.swing.JButton createBtn;
     private javax.swing.JFrame createWindow;
     private javax.swing.JButton displayBtn;
     private javax.swing.JButton editBtn;
     private javax.swing.JButton exitBtn;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JTextField gearTxt;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField speedTxt;
     private javax.swing.JLabel titleLbl;
     // End of variables declaration//GEN-END:variables
 
